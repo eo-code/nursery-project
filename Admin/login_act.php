04e1c00fe93812508480296ae55ea85dcccbb6f9
Kiputron-cpu <rifkigg24@gmail.com>
2020-09-06 10:02:47 +0700

    Crud Master_Data

--- Admin/login_act.php
+++ Admin/login_act.php
@@ -1,16 +1,15 @@
-<?php 
+<?php
 session_start();
 include 'admin/config.php';
-$username=$_POST['username'];
-$pass=$_POST['pass'];
-$pass=md5($pass);
-$query=mysql_query("select * from admin where username='$username' and pass='$pass'")or die(mysql_error());
-if(mysql_num_rows($query)==1){
-	$_SESSION['username']=$username;
+$username = $_POST['username'];
+$pass = $_POST['pass'];
+$pass = md5($pass);
+$query = mysqli_query($conect, "select * from admin where username='$username' and pass='$pass'") or die(mysqli_error());
+if (mysqli_num_rows($query) == 1) {
+	$_SESSION['username'] = $username;
 	header("location:admin/index.php");
-}else{
-	header("location:index.php?pesan=gagal")or die(mysql_error());
+} else {
+	header("location:index.php?pesan=gagal") or die(mysqli_error());
 	// mysql_error();
 }
 // echo $pas;
- ?>
